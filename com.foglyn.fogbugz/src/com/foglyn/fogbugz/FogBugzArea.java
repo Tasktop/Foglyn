f734882131caac80c895d9b0c425a4a6ba7342ae
Peter Stibrany <pstibrany@gmail.com>
2011-08-09 19:37:22 +0200

    Added EPL header to most Java files.

--- com.foglyn.fogbugz/src/com/foglyn/fogbugz/FogBugzArea.java
+++ com.foglyn.fogbugz/src/com/foglyn/fogbugz/FogBugzArea.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2008,2011 Peter Stibrany
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Peter Stibrany (pstibrany@gmail.com) - initial API and implementation
+ *******************************************************************************/
+
 package com.foglyn.fogbugz;
 
 import com.foglyn.fogbugz.FogBugzPerson.PersonID;
@@ -7,7 +18,6 @@ import com.foglyn.fogbugz.FogBugzProject.ProjectID;
  * Projects in FogBugz are divided into areas. Area belongs to project.
  * 
  * Each case has associated project as well as area.
-
  * Note: instances of this class are immutable.
  */
 public final class FogBugzArea implements HasID<FogBugzArea.AreaID> {
