17f00c1bdf6123c3cb51621033585e70a00032f0
Peter Stibrany <pstibrany@gmail.com>
2011-09-02 16:09:42 +0200

    Added EPL header from original files.

--- com.foglyn.ui/src/com/foglyn/ui/FoglynFillWidthLayout.java
+++ com.foglyn.ui/src/com/foglyn/ui/FoglynFillWidthLayout.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2010 David Green and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     David Green - initial API and implementation
+ *******************************************************************************/
+
 package com.foglyn.ui;
 
 import org.eclipse.core.runtime.Platform;
--- com.foglyn.ui/src/com/foglyn/ui/TaskEditorFogbugzEventsPart.java
+++ com.foglyn.ui/src/com/foglyn/ui/TaskEditorFogbugzEventsPart.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2010 Tasktop Technologies and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tasktop Technologies - initial API and implementation
+ *     Peter Stibrany (pstibrany@gmail.com) - Changes for Foglyn
+ *******************************************************************************/
 package com.foglyn.ui;
 
 import java.text.DateFormat;
